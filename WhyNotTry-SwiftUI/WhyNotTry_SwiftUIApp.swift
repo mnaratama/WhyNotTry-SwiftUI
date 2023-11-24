7b3ee9edaae5684c1255cbe3dbd2317d29355d87
mnaratama <mnaratama@gmail.com>
2023-11-24 13:40:24 +0700

    Initial Commit

---
+++ WhyNotTry-SwiftUI/WhyNotTry_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTry_SwiftUIApp.swift
+//  WhyNotTry-SwiftUI
+//
+//  Created by User01 on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTry_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
