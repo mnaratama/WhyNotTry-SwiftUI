993021652157f63aa1dd75f4d3c6e6e7e7a2cfa0
mnaratama <mnaratama@gmail.com>
2023-11-24 15:12:26 +0700

    Upload App

--- WhyNotTry-SwiftUI/ContentView.swift
+++ WhyNotTry-SwiftUI/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+
+    @State var selected = "Baseball"
+    @State private var id = 1
+  
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            Text("Why not try…")
+                    .font(.largeTitle.bold())
+            
+            Spacer()
+            
+            VStack{
+                Circle().fill(colors.randomElement() ?? .blue).padding().overlay(Image(systemName: "figure.\(selected.lowercased())").font(.system(size: 144)).foregroundColor(.white))
+                Text("\(selected)!")
+                    .font(.title)
+            }.transition(.slide)
+                .id(id)
+            
+            Spacer()
+            
+            Button("Try again") {
+                withAnimation(.easeInOut(duration: 1)) {
+                    selected = activities.randomElement() ?? "Archery"
+                    id += 1
+                }
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .padding()
     }
 }
 
